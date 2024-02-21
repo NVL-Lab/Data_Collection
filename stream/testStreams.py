24955a208d8af71c2e32adb081318b79e7a6bfab
Dulce Perez <95011915+dipa03@users.noreply.github.com>
2024-02-21 13:06:38 -0600

    testing function to create a stream
    https://nvl-lab.atlassian.net/browse/DC-52

--- stream/testStreams.py
+++ stream/testStreams.py
@@ -1,2 +1,21 @@
-# File to add any possible test of the stream folder functions
-## something stupid to show commit
+from stream.streamReader import RiverDataStream
+from stream.streamWriter import RiverStreamWriter
+import numpy as np
+
+def testStreams():
+    # Create and initialize the stream reader
+    with RiverDataStream(stream_name='test_stream') as stream_reader:
+        stream_reader.initialize()
+
+        # Create and initialize the stream writer
+        with RiverStreamWriter(stream_name='test_stream', sampling_rate=20) as stream_writer:
+            stream_writer.initialize()
+
+            data = np.arange(10)
+            print(data)
+
+            # Write the data to the stream
+            stream_writer.write(data)
+
+# Call the testStreams function to execute the test
+testStreams()
