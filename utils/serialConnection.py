bdd3346ad5df7371eb95403e1998609f5aef26aa
Nuria <nuria.vendrell.llopis@gmail.com>
2024-08-06 16:27:43 -0500

    class serial connection

--- utils/serialConnection.py
+++ utils/serialConnection.py
@@ -1,47 +1,34 @@
 # author: Nuria
-# This code will access the info in the serial port to read from the ESP32
-from typing import Optional
-
+# This code will access the info in the serial port to read/write from/to the ESP32
 import serial
 import time
 import warnings
+from typing import Optional
 
 
-def list_serial_ports():
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        print(f"Found port: {port.device}")
-
-
-def read_from_serial(port: str, baud_rate: int = 9600):
-    try:
-        with serial.Serial(port, baud_rate, timeout=1) as ser:
+class SerialCommunicator:
+    def __init__(self, port: str, baud_rate: int = 9600):
+        self.port = port
+        self.baud_rate = baud_rate
+        try:
+            self.ser = serial.Serial(self.port, self.baud_rate, timeout=1)
             print("Waiting for ESP32 to finish booting...")
             time.sleep(5)  # Wait for the ESP32 to finish booting
+        except serial.SerialException as e:
+            print(f"Error: {e}")
+            self.ser = None
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            self.ser = None
 
-            while True:
-                if ser.in_waiting > 0:
-                    try:
-                        data = ser.readline().decode('utf-8').rstrip()
-                        print(f"Received: {data}")
-                    except UnicodeDecodeError:
-                        data = ser.readline().decode('latin-1').rstrip()
-                        print(f"Received (decoded with latin-1): {data}")
-                    if data == 'Touch':  # to be changed by the serial message for sensor
-                        print('XXXXXXX')
-                        # do something
-                    else:  # to be deleted, only here to debug
-                        warnings.warn("Unexpected data received: {}".format(data))
-                        # do something else
-    except serial.SerialException as e:
-        print(f"Error: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    def _write_in_serial(self, command: Optional[str] = None, frequency: Optional[int] = None,
+                         duration: Optional[int] = None):
+        if self.ser is None:
+            print("Serial connection not established.")
+            return
 
-
-def write_in_serial(port: str, baud_rate: int = 9600, command:Optional[str]=None, frequency:Optional[int]=None, duration:Optional[int]=None):
-    try:
-        with serial.Serial(port, baud_rate, timeout=1) as ser:
+        t = time.time()
+        try:
             if command == "activate":
                 message = "activate"
             elif frequency is not None:
@@ -51,7 +38,56 @@ def write_in_serial(port: str, baud_rate: int = 9600, command:Optional[str]=None
             else:
                 raise ValueError("Invalid command or parameters")
 
-            ser.write(message.encode())
+            self.ser.write(message.encode())
             print(f"Sent to ESP32: {message}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        elapsed = time.time() - t
+        print(f"Elapsed time for send_command: {elapsed:.4f} seconds")
+
+    def _read_from_serial(self):
+        if self.ser is None:
+            print("Serial connection not established.")
+            return
+
+        t = time.time()
+        try:
+            while True:
+                if self.ser.in_waiting > 0:
+                    try:
+                        data = self.ser.readline().decode('utf-8').rstrip()
+                        print(f"Received: {data}")
+                    except UnicodeDecodeError:
+                        data = self.ser.readline().decode('latin-1').rstrip()
+                        print(f"Received (decoded with latin-1): {data}")
+
+                    if data == 'Touch':  # to be changed by the serial message for sensor
+                        print('XXXXXXX')
+                        # do something
+                    else:  # to be deleted, only here to debug
+                        warnings.warn("Unexpected data received: {}".format(data))
+                        # do something else
+
+                    elapsed = time.time() - t
+                    print(f"Elapsed time for read_from_serial: {elapsed:.4f} seconds")
+                    t = time.time()
+        except serial.SerialException as e:
+            print(f"Error: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+    def trigger_water_reward(self):
+        self._write_in_serial(command="activate")
+
+    def trigger_tone(self, frequency: int):
+        self._write_in_serial(frequency=frequency)
+
+    def event_touch(self):
+        self._read_from_serial()
+
+    def close_serial(self):
+        if self.ser is not None:
+            self.ser.close()
+            print("Serial connection closed.")
+        else:
+            print("Serial connection not established.")
